295a377aac366c7740e7983fc207b8d0b39c30fa
Clark Van Oyen <clark@countable.ca>
2018-08-15 15:13:34 -0700

    Update create-ssl-cert.sh

--- create-ssl-cert.sh
+++ create-ssl-cert.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Creating SSL Certificates needed for signaler and www"
-openssl req  -nodes -new -x509 -keyout localhost.key -out localhost.pem -days 365 -subj '/CN=localhost'
+openssl req  -nodes -new -x509 -keyout privkey.pem -out fullchain.pem -days 365 -subj '/CN=localhost'
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/*.key $DIR/*.pem $DIR/signaler
